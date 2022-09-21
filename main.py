0fbda9b7b329260e36761665067444ea3fcf757f
tunatuna tunatuna <113930372+Maverickstomcats@users.noreply.github.com>
2022-09-21 17:17:09 +0000

    lockheed martin

--- main.py
+++ main.py
@@ -116,7 +116,10 @@ print(control)
 # Make a comparison that returns a boolean and store the result (True/False) in a variable called test
 
 # Check if 17834/34 is greater than 87*56 and print the boolean result to the screen using print()
-
+my_number1= 17834/34 
+my_number2= 87*56
+my_results= my_number1 > my_number2
+print(my_results)
 # Check if the square root of 25 is equal to 5 and display the result (boolean) on the screen using print
 
 
